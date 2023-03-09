bb64515b29acaa78530ad030745783d7bea156bd
Emanuel-wp <emanuelwp0@gmail.com>
2023-03-08 23:17:33 -0300

    Adicionando a imagem do carro1 e do ator

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 let imagemDaEstrada;
 let imagemDoAtor;
+let imagemDoCarro1;
 
 function preload(){
     imagemDaEstrada = loadImage("imagens/estrada.png");
-    imagemDoAtor = loadImage("imagens/ator.png");
+    imagemDoAtor = loadImage("imagens/ator-1.png");
+  	imagemDoCarro1 = loadImage("imagens/carro-1.png");
+ 	
 }
 
 function setup(){
@@ -12,4 +15,6 @@ function setup(){
 
 function draw(){
     background(imagemDaEstrada);
+  image(imagemDoAtor, 100, 366, 30, 30);
+  image(imagemDoCarro1, 420, 40, 50, 40);
 } 
